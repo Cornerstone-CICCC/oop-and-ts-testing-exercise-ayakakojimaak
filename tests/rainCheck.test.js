c584246adb7f0be2ecb38b9f6f00342057a31a63
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2025-01-23 12:37:35 -0800

    add test

--- tests/rainCheck.test.js
+++ tests/rainCheck.test.js
@@ -1,30 +1,30 @@
 // Complete the test cases below by adding the expected inputs and returns
 // All test cases should pass
 
-const rainCheck = require('../src/rainCheck')
+const rainCheck = require("../src/rainCheck");
 
-describe('Weather check', () => {
+describe("Weather check", () => {
   test(`should return "Bring an umbrella" when it is raining`, () => {
-    // expect here
+    rainCheck(true, 0, 20);
   });
 
   test(`should return "No need for an umbrella" when it is not raining`, () => {
-    // expect here
+    rainCheck(false, 0, 20);
   });
 
   test(`should return "Bring a sturdy umbrella, it's windy." when it's raining and windy`, () => {
-    // expect here
+    rainCheck(true, 21, 20);
   });
 
   test(`should return "Bring an umbrella and a warm coat, it's cold." when it's raining and cold`, () => {
-    // expect here
+    rainCheck(true, 0, 9);
   });
 
   test(`should return "No umbrella needed, but stay hydrated—it's hot outside!" when it's hot and not raining`, () => {
-    // expect here
+    rainCheck(false, 0, 31);
   });
 
   test(`should return "No need for an umbrella, but it's quite windy. Hold onto your hat!" when it's windy and not raining`, () => {
-    // expect here
+    rainCheck(false, 31, 20);
   });
 });
